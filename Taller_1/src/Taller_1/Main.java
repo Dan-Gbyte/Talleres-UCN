5c2b5c97a8cc30362049427a6ae5c5cc2f175421
Demian Catalán <demiancrucesc@gmail.com>
2026-03-24 20:29:29 -0300

    Se agrega lectura de usuarios y split.

--- Taller_1/src/Taller_1/Main.java
+++ Taller_1/src/Taller_1/Main.java
@@ -1,9 +1,36 @@
 package Taller_1;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] nombres = new String[50];
+		String[] contrasenas = new String[50];
+		
+		int contPersonas = 0;
+		
+		try {
+			File file = new File("Usuarios.txt");
+			Scanner s = new Scanner(file);
+			
+			while(s.hasNextLine()){
+				String linea = s.nextLine();
+				
+				String[] partes = linea.split(";");
+				System.out.println(partes[0]);
+				
+			}
+			
+		}catch(Exception e){
+			System.out.println("Ocurrió un error al leer el archivo.." + e.getLocalizedMessage());
+			
+			
+		}
+		
+		
 		
 		System.out.println("Demian");
 		
