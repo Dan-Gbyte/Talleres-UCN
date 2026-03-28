3cbc392a39435a096ed233ecefd7bda525db8dad
Daniel Moreno <DanielG.morenom@gmail.com>
2026-03-28 02:52:08 -0300

    Lectura de ambos archivos e inicio métodos de análisis
    ahora ambos archivos .txt se leen y almacena su información correctamente, además se ha comenzado a desarrollar el método para mostrar la actividad mas realizada de cada usuario.

--- Taller_1/src/Taller_1/Main.java
+++ Taller_1/src/Taller_1/Main.java
@@ -3,6 +3,7 @@ package Taller_1;
 import java.io.File;
 import java.util.Scanner;
 
+
 public class Main {
 	
 	public static void registrar(){
@@ -20,9 +21,47 @@ public class Main {
 	public static void cambioPassword(/*password actual, nueva contraseña*/) {
 		
 	}
-
+	
+	public static void actividadMasRealizada(String[] nombres, String[] protagonista,String[] actividades, int[] horas) {
+		//sin finalizar
+		//tres for son demasiados?
+		
+		for (int i = 0; i < 50; i++) { //recorre cada usuario
+			if (nombres[i] == null) {
+				break; //terminamos si ya no hay usuarios
+			}
+			
+			String[] actividadesUnicas = new String[300];
+			int[] HorasPorActividad = new int[300];
+			
+			for (int j = 0; j< 300; j++) { //recorre los datos de cada actividad, usando de referencia al que la realiza
+				if ( protagonista[j] == null) {
+					break; //terminamos si ya no hay actividades
+					
+				} else if (nombres[i].equals(protagonista[j])) {
+					
+					for (int h = 0; h<300; h++) { //recorre las actividades unicas hasta encontrar una igual
+						
+						if (actividadesUnicas[h] != null && actividadesUnicas[h].equals(actividades[j])) { //si la actividad existe, le agregamos las horas de actividad
+							 HorasPorActividad[h] = HorasPorActividad[h] + horas[j];
+							 break;
+									 
+						} else if (actividadesUnicas[h] == null) { //si no existe, la agregamos a la lista y cortamos el for para continuar
+							actividadesUnicas[h] = actividades[j];
+							HorasPorActividad[h] = HorasPorActividad[h] + horas[j];
+							break;
+						}
+						
+					}
+				}
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
+		//quiza la lectura de este archivo deba ser en caso de abrir el menu 1
 		String[] nombres = new String[50];
 		String[] contrasenas = new String[50];
 		
@@ -50,18 +89,45 @@ public class Main {
 		
 		}
 		
+		 // rehice esto como 3 veces tratando de dejarlo listo para en los metodos hacer poco, pero me rindo, me limite a dejar cada dato en una lista
+		
+		String[] protagonista = new String[300]; //protagonista de la actividad, por supuesto
+		String[] actividades = new String[300];
+		String[] fechasAct = new String[300];
+		int[] cantidadHoras = new int[300]; //quiza deba tomarlo como string o hacer control de errores pero no es un input asi que no se si va
+		
+		
+		try {
+			File file = new File("Registros.txt");
+			Scanner s = new Scanner(file);
+			int i = 0;
+			
+			while(s.hasNextLine()){
+				String linea = s.nextLine();
+				String[] partes = linea.strip().split(";");
+				
+				protagonista[i] = partes[0];
+				fechasAct[i] = partes[1];
+				actividades[i] = partes[3];
+				cantidadHoras[i] = Integer.parseInt(partes[2]);
+				i++;
+			}
+			s.close();
+		}  catch(Exception e){
+				System.out.println("Ocurrió un error al leer el archivo.." + e.getLocalizedMessage());
+		}
+		
 		
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		
-		
 		int menu ;
 		
+		
 		do {  //do while es clave para que te siga preguntando cuando salgas de hacer algo 
+				//no lo puse porque no sabia si el menu debia continuar, aunque pensandolo el salir como opcion indica que si
 			System.out.println("1) Menu de Usuarios\r\n" + "2) Menu de Analisis\r\n" + "3) Salir");
 			try {
-				
 				menu = Integer.valueOf(entrada.nextLine()); 
 			} catch(Exception e) {
 				System.out.println("Error: Por favor solo ingresar numeros.");
@@ -94,7 +160,7 @@ public class Main {
 						
 						if (indiceUsuario <= 49 && contrasena.equals(contrasenas[indiceUsuario]) == true) {
 							acceso = true;
-							System.out.println("\n¡Acceso correcto");
+							System.out.println("\n¡Acceso correcto!");
 							
 							// AQUI IRÁ EL SUB-MENU DE USUARIOS 
 							
@@ -108,7 +174,10 @@ public class Main {
 					break; 
 					
 				case 2:
+					
 					System.out.println("\n--- Bienvenido al Menu de Analisis ---");
+					
+					
 					
 					// ACA EL OTRO MENU
 					
